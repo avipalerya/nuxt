25c5e1a23dd03207121b7ecba24974add428d8ef
Avi Palerya S R <107020@tfcorp.com>
2022-10-03 11:32:14 +0530

    nuxt firstapp

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+  <NuxtLink to="/login">Login</NuxtLink>
+  <h1>HELLO WORLD</h1>
+
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/login.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+  <h1>IM LOGIN PAGE</h1>
+  <button @click="goToHome">GO TO HOME</button>
+</div>
+</template>
+
+<script>
+export default {
+  methods:{
+    goToHome(){
+      this.$router.push("/")
+    },
+  }
+
+}
+</script>
+
+<style>
+
+</style>
